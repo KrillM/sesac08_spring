a74e275e643f8a1d69f83b0515a40c19e4fd0ec4
KrillM <mks160615@gmail.com>
2024-01-30 17:07:44 +0900

    Initial commit

---
+++ src/main/java/com/sesac/sesacspring/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.sesac.sesacspring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello") // URL 주소
+    public String getHi(Model model){ // 제어의 역전 덕분에 Model을 직접 작성하지 않는다
+        model.addAttribute("msg", "Spring Boot and Thymeleaf"); // ${msg} 통해 보여줄 내용
+        model.addAttribute("msg2", "<strong>Man</strong>"); // ${msg} 통해 보여줄 내용
+
+        model.addAttribute("position", "mid-fielder");
+        model.addAttribute("age", 16);
+
+        String[]crews = {"크릴", "예나", "지수", "수아", "이든"};
+        model.addAttribute("crews", crews);
+
+        return "hi"; // templates 이름
+    }
+}
---
+++ src/main/java/com/sesac/sesacspring/controller/Pr01Controller.java
@@ -0,0 +1,29 @@
+package com.sesac.sesacspring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+
+@Controller
+public class Pr01Controller {
+    @GetMapping("/pr01")
+    public String getPractice01(Model model){
+        model.addAttribute("age", 17);
+        return "pr01";
+    }
+
+    @GetMapping("/people")
+    public String getPractice02(Model model){
+        ArrayList<Person> people=new ArrayList<>();
+        people.add(new Person("크릴", 29));
+        people.add(new Person("예나", 22));
+        people.add(new Person("지수", 17));
+        people.add(new Person("수아", 25));
+        people.add(new Person("이든", 33));
+
+        model.addAttribute("people", people);
+        return "pr02";
+    }
+}
